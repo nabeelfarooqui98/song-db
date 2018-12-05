205afc41536d5d9f1cf10fe886d27d38c464cfac
radheem <38918435+radheem@users.noreply.github.com>
2018-12-05 19:53:08 +0500

    Add files via upload

--- firebase2.sql
+++ firebase2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 03, 2018 at 06:39 PM
+-- Generation Time: Dec 05, 2018 at 03:48 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
@@ -35,6 +35,15 @@ CREATE TABLE `album` (
   `artist_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `album`
+--
+
+INSERT INTO `album` (`album_id`, `album_name`, `releaseDate`, `artist_id`) VALUES
+(1, 'Dangerous Woman', '2018-12-04', 1),
+(18, 'grown', '1885-05-12', 1),
+(20, 'myMuazz', '2018-05-04', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -53,9 +62,10 @@ CREATE TABLE `artist` (
 --
 
 INSERT INTO `artist` (`artist_id`, `fname`, `lname`, `dateOfBirth`) VALUES
-(1, 'Ariana', 'Grande', '1993-06-26'),
+(1, 'Ariana', 'grande', '1992-12-05'),
 (2, 'Eminem', NULL, '1972-10-17'),
-(3, 'Bruno', 'Mars', '1985-10-08');
+(3, 'Bruno', 'Mars', '1985-10-08'),
+(8, 'justin', 'bieber', '1994-05-12');
 
 -- --------------------------------------------------------
 
@@ -67,6 +77,16 @@ CREATE TABLE `genre` (
   `genre_id` int(11) NOT NULL,
   `genre_name` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `genre`
+--
+
+INSERT INTO `genre` (`genre_id`, `genre_name`) VALUES
+(1, 'Pop'),
+(3, 'Rock'),
+(5, 'Rap'),
+(6, 'pop rock');
 
 -- --------------------------------------------------------
 
@@ -85,6 +105,14 @@ CREATE TABLE `song` (
   `genre_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `song`
+--
+
+INSERT INTO `song` (`song_id`, `songName`, `releaseDate`, `artist_id`, `album_id`, `musicLink`, `lyricsLink`, `genre_id`) VALUES
+(9, 'focus', '1998-12-04', 2, 1, 'https://www.youtube.com/watch?v=5OeR5XBEahU', 'https://www.youtube.com/watch?v=5OeR5XBEahU', 1),
+(11, 'let me go', '1995-12-05', 3, 18, 'https://www.youtube.com/watch?v=5OeR5XBEahU', 'https://www.youtube.com/watch?v=5OeR5XBEahU', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -96,6 +124,14 @@ CREATE TABLE `users` (
   `username` varchar(15) NOT NULL,
   `password` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`uid`, `username`, `password`) VALUES
+(1, 'nabeel', '12345'),
+(2, 'radheem', '12345');
 
 --
 -- Indexes for dumped tables
@@ -143,31 +179,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `album`
 --
 ALTER TABLE `album`
-  MODIFY `album_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `album_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `artist`
 --
 ALTER TABLE `artist`
-  MODIFY `artist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `artist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `genre`
 --
 ALTER TABLE `genre`
-  MODIFY `genre_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `genre_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `song`
 --
 ALTER TABLE `song`
-  MODIFY `song_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `song_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
